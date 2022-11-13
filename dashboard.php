843fce2d55410b6794ff06c85932d97f2902d4bd
BarbaraRSR <79948777+BarbaraRSR@users.noreply.github.com>
2022-11-13 12:28:27 -0600

    Add files via upload
    Ajuste del dashboard para mostrar información correcta con el nuevo ajuste de la base de datos.

--- dashboard.php
+++ dashboard.php
@@ -6,20 +6,22 @@ require_once 'includes/auth_check.php';
 require_once 'db/conn.php';
 
 // Get all attendees
-$results = $crud->getTicketsDashboard();
+$results = $crud->getTickets();
 ?>
 
-<a href="registration.php" class="btn btn-danger">Nuevo registro</a>
+<h1>Tickets activos</h1>
+<a href="registration.php" class="btn btn-danger">Nuevo ticket</a>
+<br>
+<HR>
 
 <table class="table">
     <thead>
         <tr>
             <th scope="col">Folio</th>
-            <th scope="col">Antigüedad</th>
+            <th scope="col">Fecha</th>
             <th scope="col">Cliente</th>
             <th scope="col">Equipo</th>
             <th scope="col">Servicio</th>
-            <th scope="col">Estatus</th>
             </th>
         </tr>
     </thead>
@@ -27,11 +29,9 @@ $results = $crud->getTicketsDashboard();
         <?php while ($r = $results->fetch(PDO::FETCH_ASSOC)) { ?>
             <tr>
                 <td><?php echo $r['folio'] ?></td>
-                <td><?php echo $r['creacion'] ?></td>
-                <td><?php echo $r['cliente'] ?></td>
-                <td><?php echo $r['equipo'] ?></td>
-                <td><?php echo $r['servicio'] ?></td>
-                <td><?php echo $r['estatus'] ?></td>
+                <td><?php echo $r['fecha'] ?></td>
+                <td><?php echo $r['nombre'] ?> <?php echo $r['apellido'] ?></td>
+                <td><?php echo $r['tipo'] ?>; <?php echo $r['marca'] ?>, <?php echo $r['modelo'] ?></td>
                 <td>
                     <a href="view.php?folio=<?php echo $r['folio'] ?>" class="btn btn-primary">Revisar</a>
                     <!--<a href="edit.php?id=<?php echo $r['folio'] ?>" class="btn btn-warning">Edit</a>
