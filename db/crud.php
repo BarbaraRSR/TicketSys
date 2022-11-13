5962d10afd4fcb0efe51db07ef5adf8fc5b26ba8
BarbaraRSR <79948777+BarbaraRSR@users.noreply.github.com>
2022-11-12 21:40:03 -0600

    Add files via upload
    Modificaciones relevantes para la nueva tabla de clientes.

--- db/crud.php
+++ db/crud.php
@@ -10,13 +10,12 @@ class crud
         $this->db = $conn;
     }
 
-
-    // function to insert a new record into the tickets database
-    public function insertTickets($cliente, $correo, $telefono, $equipo, $serie, $servicio, $estimado, $descripcion/*, $creacion*/, $estatus = "Abierto")
+    // function to insert a new record into the attendee database
+    public function insertTickets($cliente, $correo, $telefono, $equipo, $serie, $servicio, $estimado, $descripcion, $actualizado, $estatus)
     {
         try {
             // define sql statement to be executed
-            $sql = "INSERT INTO tickets(cliente,correo,telefono,equipo,serie,servicio,estimado,descripcion/*,creacion*/,estatus) VALUES (:cliente,:correo,:telefono,:equipo,:serie,:servicio,:estimado,:descripcion/*,:creacion*/,:estatus)";
+            $sql = "INSERT INTO tickets(cliente,correo,telefono,equipo,serie,servicio,estimado,descripcion,actualizado,estatus) VALUES (:cliente,:correo,:telefono,:equipo,:serie,:servicio,:estimado,:descripcion,:actualizado,:estatus)";
             // prepare the sql statement for execution
             $stmt = $this->db->prepare($sql);
             //bind all placeholders to the actual values
@@ -28,7 +27,7 @@ class crud
             $stmt->bindparam(':servicio', $servicio);
             $stmt->bindparam(':estimado', $estimado);
             $stmt->bindparam(':descripcion', $descripcion);
-            //$stmt->bindparam(':creacion', $creacion);
+            $stmt->bindparam(':actualizado', $actualizado);
             $stmt->bindparam(':estatus', $estatus);
             // execute statement
             $stmt->execute();
@@ -39,23 +38,19 @@ class crud
         }
     }
 
-    public function editTicket($folio, $cliente, $correo, $telefono, $equipo, $serie, $servicio, $estimado, $descripcion, $creacion, $estatus)
+    public function editAttendee($id, $fname, $lname, $dob, $email, $contact, $specialty)
     {
         try {
-            $sql = "UPDATE `tickets` SET `cliente`=:cliente,`correo`=:correo,`telefono`=:telefono,`equipo`=:equipo,`serie`=:serie,`servicio`=:servicio,`estimado`=:estimado,`descripcion`=:descripcion,`creacion`=:creacion,`estatus`=:estatus WHERE folio = :folio";
+            $sql = "UPDATE `attendee` SET `firstname`=:fname,`lastname`=:lname,`dateofbirth`=:dob,`emailaddress`=:email,`contactnumber`=:contact,`specialty_id`=:specialty WHERE attendee_id = :id";
             $stmt = $this->db->prepare($sql);
             //bind all placeholders to the actual values
-            $stmt->bindparam(':folio', $folio);
-            $stmt->bindparam(':cliente', $cliente);
-            $stmt->bindparam(':correo', $correo);
-            $stmt->bindparam(':telefono', $telefono);
-            $stmt->bindparam(':equipo', $equipo);
-            $stmt->bindparam(':serie', $serie);
-            $stmt->bindparam(':servicio', $servicio);
-            $stmt->bindparam(':estimado', $estimado);
-            $stmt->bindparam(':descripcion', $descripcion);
-            $stmt->bindparam(':creacion', $creacion);
-            $stmt->bindparam(':estatus', $estatus);
+            $stmt->bindparam(':id', $id);
+            $stmt->bindparam(':fname', $fname);
+            $stmt->bindparam(':lname', $lname);
+            $stmt->bindparam(':dob', $dob);
+            $stmt->bindparam(':email', $email);
+            $stmt->bindparam(':contact', $contact);
+            $stmt->bindparam(':specialty', $specialty);
             // execute statement
             $stmt->execute();
             return true;
@@ -65,7 +60,19 @@ class crud
         }
     }
 
-    public function getTicketsDashboard()
+    public function getClients()
+    {
+        try {
+            $sql = "SELECT * FROM clients";
+            $result = $this->db->query($sql);
+            return $result;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            return false;
+        }
+    }
+
+    public function getTickets()
     {
         try {
             $sql = "SELECT * FROM tickets WHERE estatus = 'abierto'";
@@ -77,11 +84,14 @@ class crud
         }
     }
 
-    public function getTicketsHistory()
+    public function getClientDetails($clienteid)
     {
         try {
-            $sql = "SELECT * FROM tickets";
-            $result = $this->db->query($sql);
+            $sql = "SELECT * FROM clients WHERE clienteid = :clienteid";
+            $stmt = $this->db->prepare($sql);
+            $stmt->bindparam(':clienteid', $clienteid);
+            $stmt->execute();
+            $result = $stmt->fetch();
             return $result;
         } catch (PDOException $e) {
             echo $e->getMessage();
@@ -104,12 +114,30 @@ class crud
         }
     }
 
-    public function deleteTicket($folio)
+
+    public function getAttendeeDetails($id)
     {
         try {
-            $sql = "DELETE FROM tickets WHERE folio = :folio";
+            $sql = "SELECT * FROM attendee a inner join specialties s on a.specialty_id = s.specialty_id WHERE attendee_id = :id";
             $stmt = $this->db->prepare($sql);
-            $stmt->bindparam(':folio', $folio);
+            $stmt->bindparam(':id', $id);
+            $stmt->execute();
+            $result = $stmt->fetch();
+            return $result;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            return false;
+        }
+    }
+
+
+
+    public function deleteAttendee($id)
+    {
+        try {
+            $sql = "DELETE FROM attendee WHERE attendee_id = :id";
+            $stmt = $this->db->prepare($sql);
+            $stmt->bindparam(':id', $id);
             $stmt->execute();
             return true;
         } catch (PDOException $e) {
