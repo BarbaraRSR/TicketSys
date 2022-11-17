b7b18a1bdc0eb2e05f0da062447ab0fa9aaaf65d
BarbaraRSR <79948777+BarbaraRSR@users.noreply.github.com>
2022-11-17 16:17:49 -0600

    Favicon
    Se agrega el favicon.

--- includes/header.php
+++ includes/header.php
@@ -13,6 +13,7 @@ include_once 'includes/redirect.php';
     <!-- Required meta tags -->
     <meta http-equiv=”Content-Type” content=”text/html; charset=ISO-8859-1″ />
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="https://www.laptown.com/wp-content/themes/laptown-v2/favico_16.png" type="image/x-icon">
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
