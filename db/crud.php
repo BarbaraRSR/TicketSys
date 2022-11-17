9e30d0b767389c26933bcfce6190d000b0442957
BarbaraRSR <79948777+BarbaraRSR@users.noreply.github.com>
2022-11-16 18:16:24 -0600

    Add files via upload

--- db/crud.php
+++ db/crud.php
@@ -3,7 +3,6 @@ class crud
 {
     // private database object
     private $db;
-    protected $numPaginas;
 
     //constructor to initialize private variable to the database connection
     function __construct($conn)
@@ -11,47 +10,43 @@ class crud
         $this->db = $conn;
     }
 
-    // function to insert a new record into the attendee database
-    public function insertClients($nombre, $apellido, $telefono, $correo, $comentarios)
-    {
-        try {
-            // define sql statement to be executed
-            $sql = "INSERT INTO clients(nombre,apellido,telefono,correo,comentarios)
-                    VALUES (:nombre,:apellido,:telefono,:correo,:comentarios)";
-            // prepare the sql statement for execution
-            $stmt = $this->db->prepare($sql);
-            //bind all placeholders to the actual values
-            $stmt->bindparam(':nombre', $nombre);
-            $stmt->bindparam(':apellido', $apellido);
-            $stmt->bindparam(':telefono', $telefono);
-            $stmt->bindparam(':correo', $correo);
-            $stmt->bindparam(':comentarios', $comentarios);
-            // execute statement
-            $stmt->execute();
-            return true;
-        } catch (PDOException $e) {
-            echo $e->getMessage();
-            return false;
-        }
-    }
+/////////////
+/* TICKETS */
+///////////// 
 
-    public function insertTickets($clienteid, $tipo, $marca, $modelo, $serie, $servicio, $estimado, $descripcion)
+public function insertTicket($cliente, $equipo, $servicio, $estimado, $descripcion/*, $fecha*/, $estatus = "Abierto")
+{
+    try {
+        $sql = "INSERT INTO tickets(cliente_id, equipo_id, servicio,estimado,descripcion/*,fecha*/,estatus) VALUES (:cliente, :equipo, :servicio,:estimado,:descripcion/*,:fecha*/,:estatus)";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindparam(':cliente', $cliente);
+        $stmt->bindparam(':equipo', $equipo);
+        $stmt->bindparam(':servicio', $servicio);
+        $stmt->bindparam(':estimado', $estimado);
+        $stmt->bindparam(':descripcion', $descripcion);
+        //$stmt->bindparam(':fecha', $fecha);
+        $stmt->bindparam(':estatus', $estatus);
+        $stmt->execute();
+        return true;
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+        return false;
+    }
+}
+
+    public function editTicket($folio, $cliente, $equipo, $servicio, $estimado, $descripcion, $fecha, $estatus)
     {
         try {
-            // define sql statement to be executed
-            $sql = "INSERT INTO tickets(clienteid,tipo,marca,modelo,serie,servicio,estimado,descripcion) VALUES (:clienteid,:tipo,:marca,:modelo,:serie,:servicio,:estimado,:descripcion)";
-            // prepare the sql statement for execution
+            $sql = "UPDATE `tickets` SET `cliente_id`=:cliente,`equipo_id`=:equipo,`servicio`=:servicio,`estimado`=:estimado,`descripcion`=:descripcion,`fecha`=:fecha,`estatus`=:estatus WHERE folio = :folio";
             $stmt = $this->db->prepare($sql);
-            //bind all placeholders to the actual values
-            $stmt->bindparam(':clienteid', $clienteid);
-            $stmt->bindparam(':tipo', $correo);
-            $stmt->bindparam(':marca', $telefono);
-            $stmt->bindparam(':modelo', $equipo);
-            $stmt->bindparam(':serie', $serie);
+            $stmt->bindparam(':folio', $folio);
+            $stmt->bindparam(':cliente', $cliente);
+            $stmt->bindparam(':equipo', $equipo);
             $stmt->bindparam(':servicio', $servicio);
             $stmt->bindparam(':estimado', $estimado);
             $stmt->bindparam(':descripcion', $descripcion);
-            // execute statement
+            $stmt->bindparam(':fecha', $fecha);
+            $stmt->bindparam(':estatus', $estatus);
             $stmt->execute();
             return true;
         } catch (PDOException $e) {
@@ -60,124 +55,28 @@ class crud
         }
     }
 
-    public function getClients($total, $pagina, $registros = 10)
+    public function getTickets()
     {
         try {
-            $this->numPaginas = ceil($total/$registros); 
-            $inicio = ($registros*$pagina)-$registros; 
-
-            $sql = "SELECT * FROM clients limit $inicio,$registros";
-            $result = $this->db->query($sql);
-            return $result;
-        } catch (PDOException $e) {
-            echo $e->getMessage();
-            return false;
-        }
-    }
-
-    public function getClientsAll()
-    {
-        try {
-            $sql = "SELECT count(*) AS total FROM clients";
-            $result = $this->db->query($sql);
-            $row = $result->fetch(PDO::FETCH_ASSOC);
-            return $row['total'];
-        } catch (PDOException $e) {
-            echo $e->getMessage();
-            return false;
-        }
-    }
-
-    public function getTickets($total, $pagina, $registros = 10)
-    {
-        try {
-            $this->numPaginas = ceil($total/$registros); 
-            $inicio = ($registros*$pagina)-$registros; 
-
-            $sql = "SELECT folio, fecha, tipo, marca, modelo, servicio, clients.clienteid, clients.nombre, clients.apellido
-                    FROM tickets
-                    INNER JOIN clients ON tickets.clienteid=clients.clienteid
-                    WHERE estatus = 'abierto' limit $inicio,$registros";
-            $result = $this->db->query($sql);
-            return $result;
-        } catch (PDOException $e) {
-            echo $e->getMessage();
-            return false;
-        }
-    }
-
-    public function getTicketsAll()
-    {
-        try {
-            $sql = "SELECT count(*) AS total 
-                    FROM tickets
-                    INNER JOIN clients ON tickets.clienteid=clients.clienteid
+            $sql = "SELECT * FROM tickets t 
+                    inner join clients c on t.cliente_id = c.cliente_id
+                    inner join devices d on t.equipo_id = d.equipo_id 
                     WHERE estatus = 'abierto'";
-            $result = $this->db->query($sql);
-            $row = $result->fetch(PDO::FETCH_ASSOC);
-            return $row['total'];
+            $resultado = $this->db->query($sql);
+            return $resultado;
         } catch (PDOException $e) {
             echo $e->getMessage();
             return false;
         }
     }
 
-    public function getHistory($total, $pagina, $registros = 10)
+    public function getTicketsALL()
     {
         try {
-            $this->numPaginas = ceil($total/$registros); 
-            $inicio = ($registros*$pagina)-$registros; 
-
-            $sql = "SELECT folio, fecha, tipo, marca, modelo, servicio, estimado, estatus, clients.clienteid, clients.nombre, clients.apellido
-                        FROM tickets
-                        INNER JOIN clients ON tickets.clienteid=clients.clienteid  limit $inicio, $registros";
+            $sql = "SELECT * FROM tickets t 
+                    inner join clients c on t.cliente_id = c.cliente_id
+                    inner join devices d on t.equipo_id = d.equipo_id ";
             $result = $this->db->query($sql);
-            return $result;
-        } catch (PDOException $e) {
-            echo $e->getMessage();
-            return false;
-        }
-    }
-
-
-    public function getHistoryAll()
-    {
-        try {
-            $sql = "SELECT count(*) AS total FROM tickets
-                        INNER JOIN clients ON tickets.clienteid=clients.clienteid;";
-            $result = $this->db->query($sql);
-            $row = $result->fetch(PDO::FETCH_ASSOC);
-            return $row['total'];
-        } catch (PDOException $e) {
-            echo $e->getMessage();
-            return false;
-        }
-    }
-
-
-    public function getPaginator($pagina)
-    {
-        $paginator = '';
-        if ($this->numPaginas > 1) {
-            $paginator .= '<nav aria-label="Page navigation example">
-        <ul class="pagination">';
-            for($i = 1; $i < $this->numPaginas + 1; $i++) { 
-                $paginator .= "<li class='page-item'><a class='page-link' href='".$pagina."?pagina=$i'>".$i."</a></li>"; 
-            } 
-            $paginator .= '</ul>
-            </nav>';
-        }
-        return $paginator;
-    }
-
-    public function getClientDetails($clienteid)
-    {
-        try {
-            $sql = "SELECT * FROM clients WHERE clienteid = :clienteid";
-            $stmt = $this->db->prepare($sql);
-            $stmt->bindparam(':clienteid', $clienteid);
-            $stmt->execute();
-            $result = $stmt->fetch();
             return $result;
         } catch (PDOException $e) {
             echo $e->getMessage();
@@ -188,9 +87,10 @@ class crud
     public function getTicketDetails($folio)
     {
         try {
-            $sql = "SELECT folio, fecha, tipo, marca, modelo, serie, descripcion, servicio, estimado, estatus, clients.clienteid, clients.nombre, clients.apellido, clients.telefono, clients.correo, clients.comentarios
-            FROM tickets
-            INNER JOIN clients ON tickets.clienteid=clients.clienteid;";
+            $sql = "SELECT * FROM tickets t 
+                    inner join clients c on t.cliente_id = c.cliente_id 
+                    inner join devices d on t.equipo_id = d.equipo_id 
+                    WHERE folio = :folio";
             $stmt = $this->db->prepare($sql);
             $stmt->bindparam(':folio', $folio);
             $stmt->execute();
@@ -202,13 +102,12 @@ class crud
         }
     }
 
-
-    public function deleteAttendee($id)
+    public function deleteTicket($folio)
     {
         try {
-            $sql = "DELETE FROM attendee WHERE attendee_id = :id";
+            $sql = "DELETE FROM tickets WHERE folio = :folio";
             $stmt = $this->db->prepare($sql);
-            $stmt->bindparam(':id', $id);
+            $stmt->bindparam(':folio', $folio);
             $stmt->execute();
             return true;
         } catch (PDOException $e) {
@@ -217,23 +116,174 @@ class crud
         }
     }
 
-    /* ESTO NO PARECE SER PARTE DE NUESTRO SISTEMA
-    public function getSpecialties()
+/////////////
+/* CLIENTS */
+///////////// 
+
+    public function insertClient($nombre, $apellido, $telefono, $correo, $comentarios)
     {
         try {
-            $sql = "SELECT * FROM `specialties`";
+            $sql = "INSERT INTO clients(nombre,apellido,telefono,correo,comentarios)
+                    VALUES (:nombre,:apellido,:telefono,:correo,:comentarios)";
+            $stmt = $this->db->prepare($sql);
+            $stmt->bindparam(':nombre', $nombre);
+            $stmt->bindparam(':apellido', $apellido);
+            $stmt->bindparam(':telefono', $telefono);
+            $stmt->bindparam(':correo', $correo);
+            $stmt->bindparam(':comentarios', $comentarios);
+            $stmt->execute();
+            return true;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            return false;
+        }
+    }
+
+    public function editClient($cliente_id, $nombre, $apellido, $telefono, $correo, $comentarios)
+    {
+        try {
+            $sql = "UPDATE `clients` SET `nombre`=:nombre,`apellido`=:apellido,`telefono`=:telefono,`correo`=:correo,`comentarios`=:comentarios WHERE cliente_id = :cliente_id";
+            $stmt = $this->db->prepare($sql);
+            $stmt->bindparam(':cliente_id', $cliente_id);
+            $stmt->bindparam(':nombre', $nombre);
+            $stmt->bindparam(':apellido', $apellido);
+            $stmt->bindparam(':telefono', $telefono);
+            $stmt->bindparam(':correo', $correo);
+            $stmt->bindparam(':comentarios', $comentarios);
+            $stmt->execute();
+            return true;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            return false;
+        }
+    }
+
+    public function getClients()
+    {
+        try {
+            $sql = "SELECT * FROM clients";
             $result = $this->db->query($sql);
             return $result;
         } catch (PDOException $e) {
             echo $e->getMessage();
             return false;
         }
-    } 
+    }
 
-    public function getSpecialtyById($id)
+    public function getClientDetails($cliente_id)
     {
         try {
-            $sql = "SELECT * FROM `specialties` where specialty_id = :id";
+            $sql = "SELECT * FROM clients WHERE cliente_id = :cliente_id";
+            $stmt = $this->db->prepare($sql);
+            $stmt->bindparam(':cliente_id', $cliente_id);
+            $stmt->execute();
+            $result = $stmt->fetch();
+            return $result;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            return false;
+        }
+    }
+
+    public function deleteClient($cliente_id)
+    {
+        try {
+            $sql = "DELETE FROM clients WHERE cliente_id = :cliente_id";
+            $stmt = $this->db->prepare($sql);
+            $stmt->bindparam(':cliente_id', $cliente_id);
+            $stmt->execute();
+            return true;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            return false;
+        }
+    }
+
+/////////////
+/* DEVICES */
+///////////// 
+
+public function insertDevice($tipo, $marca, $modelo, $serie)
+{
+    try {
+        $sql = "INSERT INTO devices(tipo,marca,modelo,serie)
+                VALUES (:tipo,:marca,:modelo,:serie)";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindparam(':tipo', $tipo);
+        $stmt->bindparam(':marca', $marca);
+        $stmt->bindparam(':modelo', $modelo);
+        $stmt->bindparam(':serie', $serie);
+        $stmt->execute();
+        return true;
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+        return false;
+    }
+}
+
+public function editDevice($equipo_id, $tipo, $marca, $modelo, $serie)
+{
+    try {
+        $sql = "UPDATE `devices` SET `tipo`=:tipo,`marca`=:marca,`modelo`=:modelo,`serie`=:serie WHERE equipo_id = :equipo_id";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindparam(':equipo_id', $equipo_id);
+        $stmt->bindparam(':tipo', $tipo);
+        $stmt->bindparam(':marca', $marca);
+        $stmt->bindparam(':modelo', $modelo);
+        $stmt->bindparam(':serie', $serie);
+        $stmt->execute();
+        return true;
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+        return false;
+    }
+}
+
+    public function getDevices()
+    {
+        try {
+            $sql = "SELECT * FROM `devices`";
+            $result = $this->db->query($sql);
+            return $result;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            return false;
+        }
+    }
+
+    public function getDeviceDetails($equipo_id)
+    {
+        try {
+            $sql = "SELECT * FROM devices WHERE equipo_id = :equipo_id";
+            $stmt = $this->db->prepare($sql);
+            $stmt->bindparam(':equipo_id', $equipo_id);
+            $stmt->execute();
+            $result = $stmt->fetch();
+            return $result;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            return false;
+        }
+    }
+
+    public function deleteDevice($equipo_id)
+    {
+        try {
+            $sql = "DELETE FROM devices WHERE equipo_id = :equipo_id";
+            $stmt = $this->db->prepare($sql);
+            $stmt->bindparam(':equipo_id', $equipo_id);
+            $stmt->execute();
+            return true;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            return false;
+        }
+    }
+
+    public function getDevicesById($id)
+    {
+        try {
+            $sql = "SELECT * FROM `devices` where equipo_id = :id";
             $stmt = $this->db->prepare($sql);
             $stmt->bindparam(':id', $id);
             $stmt->execute();
@@ -243,7 +293,9 @@ class crud
             echo $e->getMessage();
             return false;
         }
-    } */
+    }
+
+
 }
 
 ?>
