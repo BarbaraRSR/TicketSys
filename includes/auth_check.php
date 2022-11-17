9c4a2c5fcd82894a1a62873f7d57bb582776ea87
BarbaraRSR <79948777+BarbaraRSR@users.noreply.github.com>
2022-11-16 20:00:40 -0600

    Add files via upload

--- includes/auth_check.php
+++ includes/auth_check.php
@@ -1,5 +1,12 @@
 <?php 
     if(!isset($_SESSION['userid'])){
-        header("Location: index.php");
+        //header("Location: index.php");
+        if ($_SERVER['HTTP_HOST'] == "localhost") {
+            header("Location: index.php");
+        } else {
+          echo '<script type="text/javascript">
+          alert("Acción exitosa");
+          window.location.assign("index.php");</script>';        
+        }
     }
 ?>
