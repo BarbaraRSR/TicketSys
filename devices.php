98de004b248e414a05eaf9d3cd42cc19ea4fd285
BarbaraRSR <79948777+BarbaraRSR@users.noreply.github.com>
2022-11-16 20:59:53 -0600

    Sin S/N
    Se retira el número de serie ("serie") de la tabla de dispositivos.

--- devices.php
+++ devices.php
@@ -34,8 +34,7 @@ $results = $crud->getDevices();
             <th scope="col">Tipo</th>
             <th scope="col">Marca</th>
             <th scope="col">Modelo</th>
-            <th scope="col">Serie</th>
-            </th>
+            <th scope="col">Acciones</th>
         </tr>
     </thead>
     <tbody>
@@ -45,7 +44,6 @@ $results = $crud->getDevices();
                 <td><?php echo $r['tipo'] ?></td>
                 <td><?php echo $r['marca'] ?></td>
                 <td><?php echo $r['modelo'] ?></td>
-                <td><?php echo $r['serie'] ?></td>
                 <td>
                     <a href="deviceedit.php?equipo_id=<?php echo $r['equipo_id'] ?>" class="btn btn-warning">Actualizar</a>
                     <a onclick="return confirm('¿Desea eliminar permanentemente a este equipo?');" href="devicedelete.php?equipo_id=<?php echo $r['equipo_id'] ?>" class="btn btn-danger">Borrar</a>
