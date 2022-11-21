68ed0b5e501a22f02a96f38ad803436db0df9b4f
BarbaraRSR <79948777+BarbaraRSR@users.noreply.github.com>
2022-11-20 19:47:22 -0600

    Add files via upload

---
+++ includes/buttons.php
@@ -0,0 +1,9 @@
+<!-- Add TICKET -->
+<a href="<?php page('ticketnew.php')?>" class="btn btn-success" title="Agregar ticket">
+    <img src="img/plus.svg" width="23"> <img src="img/ticket.svg" width="23"></a>
+<!-- Add CLIENT -->
+<a href="<?php page('clientnew.php')?>" class="btn btn-success" title="Agregar cliente">
+    <img src="img/plus.svg" width="23"> <img src="img/user.svg" width="23"></a>
+<!-- Add DEVICE -->
+<a href="<?php page("devicenew.php")?>" class="btn btn-success" title="Agregar equipo">
+    <img src="img/plus.svg" width="23"> <img src="img/pc.svg" width="23"></a><br>
