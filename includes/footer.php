4e095e1fbfba8a73e8f9dd1ebcc4d4ff84533014
BarbaraRSR <79948777+BarbaraRSR@users.noreply.github.com>
2022-11-22 19:36:25 -0600

    Add files via upload

--- includes/footer.php
+++ includes/footer.php
@@ -3,7 +3,7 @@
 <br>
 
   <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-    <div class="col-md-4 d-flex align-items-center" id="footer">
+    <div class="d-flex align-items-center" id="footer">
       <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
         <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
       </a>
