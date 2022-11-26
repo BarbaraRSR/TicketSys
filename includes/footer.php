9a8167fdd05f9e88bdcea7e4befc93a6ea0cac13
BarbaraRSR <79948777+BarbaraRSR@users.noreply.github.com>
2022-11-25 19:02:31 -0600

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -7,7 +7,7 @@
       <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
         <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
       </a>
-      <span class="text-muted"><?php echo '© Laptown ' . date("Y"); ?> - TicketSys versión 4 (2022)</span>
+      <span class="text-muted"><?php echo '© Laptown ' . date("Y"); ?> - TicketSys versión 5.2 (2022)</span>
     </div>
   </footer>
 
